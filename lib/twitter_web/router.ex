20469cb13dadbaa2624ed06a4dfb226583bcfc58
Sergey <mk-serg@yandex.ru>
2019-06-01 21:53:40 +0300

    CRUD users

--- lib/twitter_web/router.ex
+++ lib/twitter_web/router.ex
@@ -2,21 +2,22 @@ defmodule TwitterWeb.Router do
   use TwitterWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", TwitterWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+    resources("/users", UserController)
   end
 
   # Other scopes may use custom stacks.
